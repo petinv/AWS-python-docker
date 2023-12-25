9b9b4339840d723a2dd5a32729bf74b055d55bc6
Slava <petinvv@gmail.com>
2023-12-25 15:16:55 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def greet():
+    name = request.form['name']
+    greeting = f'Привет, {name}!'
+    return render_template('index.html', greeting=greeting)
+
+if __name__ == '__main__':
+    app.run(debug=True)
