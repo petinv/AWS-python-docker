5e7e8bc343b3a4686dba089b567a37da7394ec1c
Slava <petinvv@gmail.com>
2024-01-16 00:02:51 +0200

    update start port

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def greet():
     return render_template('index.html', greeting=greeting)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
 
